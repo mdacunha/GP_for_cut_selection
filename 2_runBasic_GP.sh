fc093d7e8eb2bcaa87725bb67902db8d8398a99b
Mathisdac <math.dac1@gmail.com>
2025-05-27 14:35:38 +0200

    .

--- 2_runBasic_GP.sh
+++ 2_runBasic_GP.sh
@@ -1,52 +1,56 @@
 #!/bin/bash -l
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=50
 #SBATCH -c 1
-#SBATCH --time=9-00:00:00
+#SBATCH --time=0-10:00:00
 #SBATCH -p batch
-#SBATCH --qos=long
+##SBATCH --qos=long
 #SBATCH --output=3_Basic_GP.out
 #SBATCH --error=3_Basic_GP.err
 
+# Aller dans le répertoire de travail
 cd /mnt/aiongpfs/users/mdacunha/GP_for_cut_selection/ || { echo "Failed to change directory"; exit 1; }
 
 # Initialiser micromamba
 eval "$(micromamba shell hook --shell bash)" || { echo "Failed to initialize Micromamba"; exit 1; }
-
 micromamba activate GP_for_cut_selection || { echo "Failed to activate Micromamba environment"; exit 1; }
 
 export SLURM_CPU_BIND=none
+export SLURM_NTASKS=50
 
-HOSTFILE=$(pwd)/hostfile
+# Créer le wrapper Python avec environnement activé
 SCOOP_WRAPPER=$(pwd)/scoop-python.sh
-
-cat << EOF > $SCOOP_WRAPPER
+cat << 'EOF' > "$SCOOP_WRAPPER"
 #!/bin/bash -l
-eval "\$(micromamba shell hook --shell=bash)"
+eval "$(micromamba shell hook --shell=bash)"
 micromamba activate GP_for_cut_selection
+python "$@"
 EOF
-echo 'python $@' >> $SCOOP_WRAPPER
+chmod +x "$SCOOP_WRAPPER"
 
-chmod +x $SCOOP_WRAPPER
+# Créer un fichier hostfile à partir de SLURM
+HOSTFILE=$(pwd)/hostfile
+scontrol show hostnames > "$HOSTFILE"
 
-scontrol show hostnames > $HOSTFILE
-
+# Log infos job
 echo "Début du job à $(date)"
 echo "ID du job : ${SLURM_JOBID}"
 echo "Répertoire de soumission : ${SLURM_SUBMIT_DIR}"
 
+# Ajouter répertoire au PYTHONPATH si nécessaire
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
+# Vérification des arguments
 if [ -z "$1" ]; then
-  echo "Arguments non fournis. Utilisation : sbatch script.sh <argument> <argument>"
+  echo "Arguments non fournis. Utilisation : sbatch run_gp_scoop.sh <argument>"
   exit 1
 fi
 
 ARGUMENT1=$1
-
-# Exécuter le script avec SCOOP
 INPUTFILE=$(pwd)/basic_main.py
-python -m scoop --hostfile $HOSTFILE -n ${SLURM_NTASKS} --python-interpreter=$SCOOP_WRAPPER $INPUTFILE "$ARGUMENT1" None
+
+# Lancer le script avec SCOOP
+python -m scoop --hostfile "$HOSTFILE" -n "$SLURM_NTASKS" --python-interpreter="$SCOOP_WRAPPER" "$INPUTFILE" "$ARGUMENT1" None
 
 echo "Fin du job à $(date)"
 
